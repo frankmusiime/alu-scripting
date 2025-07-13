c1732627ceea89f183c39d9a5c62d4f7718b0f9a
frankmusiime <f.musiime@alustudent.com>
2025-07-12 23:13:30 -0700

    my advanced api

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -6,43 +6,16 @@ import requests
 
 
 def top_ten(subreddit):
-    """
-    Prints the titles of the first 10 hot posts for a given subreddit.
-    
-    Args:
-        subreddit (str): The name of the subreddit
-        
-    Returns:
-        None: Prints titles or None if subreddit is invalid
-    """
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    """Prints the top ten hot posts for a given subreddit"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
-    
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        if response.status_code != 200:
-            print(None)
-            return
-            
-        json_data = response.json()
-        if not json_data or 'data' not in json_data:
-            print(None)
-            return
-            
-        data = json_data['data']
-        children = data.get('children', [])
-        
-        if not children:
-            print(None)
-            return
-            
-        # Print titles of first 10 posts
-        for post in children[:10]:
-            post_data = post.get('data', {})
-            title = post_data.get('title', '')
-            if title:
-                print(title)
-            
-    except (requests.RequestException, ValueError, KeyError):
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
         print(None)
+        return
+    data = response.json().get("data")
+    if data is None or len(data.get("children")) == 0:
+        print(None)
+        return
+    for child in data.get("children"):
+        print(child.get("data").get("title"))
